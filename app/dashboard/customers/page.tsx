3126ce78b830cd08d5297b29e8c607cee7149188
headlessdemomatrix <headlessdemo@matrixconsulting.it>
2024-09-18 15:29:42 +0200

    Aggiunta tabella customers in Customers Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { CustomerSkeleton } from '@/app/ui/skeletons';
+import Customers from '@/app/ui/customers/customers';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-    return <p>Customers Page</p>
+    return (
+      <main>
+        <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-1">
+          <Suspense fallback={<CustomerSkeleton />}>
+              <Customers/>
+          </Suspense>
+        </div>
+      </main>
+    );
 }
